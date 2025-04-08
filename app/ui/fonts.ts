fb40d2af52c563d5854e4f6988103f8e6046c397
faithgaiciumia <faithgaiciumia078@gmail.com>
2025-04-08 20:41:56 +0300

    added fonts

---
+++ app/ui/fonts.ts
@@ -0,0 +1,21 @@
+import {
+    Poppins,
+    Playfair_Display,
+    Merriweather,
+    Lato,
+    Work_Sans,
+    Inter,
+  } from "next/font/google";
+  export const poppins = Poppins({
+    weight: ["600"],
+    subsets: ["latin"],
+  });
+  export const workSans = Work_Sans({
+      weight:["500"],
+      subsets:["latin"]
+  });
+  export const inter = Inter({
+      weight:["400"],
+      subsets:["cyrillic"]
+  });
+  
