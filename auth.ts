2892e674ba601985dec0e3369d7b97f4853e840a
faithgaiciumia <faithgaiciumia078@gmail.com>
2025-04-09 23:26:49 +0300

    login with email resend basic setup

--- auth.ts
+++ auth.ts
@@ -1,5 +1,27 @@
+import NeonAdapter from "@auth/neon-adapter";
+import { Pool } from "@neondatabase/serverless";
 import NextAuth from "next-auth";
+import Resend from "next-auth/providers/resend";
 
-export const {handlers,signIn,signOut,auth}=NextAuth({
-    providers:[]
-})
+export const { handlers, auth, signIn, signOut } = NextAuth(() => {
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+  return {
+    adapter: NeonAdapter(pool),
+    providers: [Resend({ from: "Risity@login.gaiciumiafaith.com" })],
+    callbacks: {
+      async jwt({ token, account, user }) {
+        if (account?.provider === "resend" && user) {
+          token.email = user.email;
+        }
+        return token;
+      },
+
+      async session({ session, token }) {
+        return session;
+      },
+    },
+    session: {
+      strategy: "jwt",
+    },
+  };
+});
