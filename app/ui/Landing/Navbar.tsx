e7878790ccb05024cd6f28910cfc29ec83843897
faithgaiciumia <faithgaiciumia078@gmail.com>
2025-04-09 12:47:34 +0300

    added login button

--- app/ui/Landing/Navbar.tsx
+++ app/ui/Landing/Navbar.tsx
@@ -40,9 +40,11 @@ export default function Navbar() {
               </li>
             </ul>
             <div className="flex space-x-4">
-              <button className="px-4 py-2 border border-green-500 text-green-500 rounded-lg hover:bg-green-50 transition">
-                Log in
-              </button>
+              <Link href={"/login"}>
+                <button className="px-4 py-2 border border-green-500 text-green-500 rounded-lg hover:bg-green-50 transition">
+                  Log in
+                </button>
+              </Link>
               <button className="px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600 transition">
                 Sign up
               </button>
