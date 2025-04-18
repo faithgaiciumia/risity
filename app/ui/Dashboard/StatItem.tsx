01f89857b0867e9d1659adb806c3dceae7a1a166
faithgaiciumia <faithgaiciumia078@gmail.com>
2025-04-18 05:37:49 +0300

    broke stats list into reusable component

---
+++ app/ui/Dashboard/StatItem.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+
+export default function StatItem({icon, label, value}:{icon:ReactNode, label:string, value:string}) {
+  return (
+    <div className="flex justify-between items-center">
+      <div className="flex items-center space-x-2">
+        <span className="text-sm">{icon}</span>
+        <p className="text-sm">{label}</p>
+      </div>
+      <p className="text-sm text-gray-500">{value}</p>
+    </div>
+  );
+}
--- app/ui/Dashboard/StatsOverview.tsx
+++ app/ui/Dashboard/StatsOverview.tsx
@@ -1,21 +1,34 @@
+import { FaChartLine, FaMoneyBillWave, FaUsers } from "react-icons/fa6";
 import { workSans } from "../fonts";
+import StatItem from "./StatItem";
 
-export default function StatsOverview(){
-    return(
-        <div className="bg-white border border-gray-300 p-4 w-full md:w-1/2">
-          {/* heading */}
-          <div className="flex items-center justify-between">
-            <h1 className={`text-md font-bold ${workSans.className}`}>
-              Stats at a glance
-            </h1>
-          </div>
-          {/* stats list */}
-          <div className="my-2">
-            <div className="flex justify-between items-center">
-              <p className="text-sm">Monthly Average Revenue</p>
-              <p className="text-sm text-gray-500">12,000 kes</p>
-            </div>
-          </div>
-        </div>
-    )
+export default function StatsOverview() {
+  return (
+    <div className="bg-white border border-gray-300 p-4 w-full md:w-1/2">
+      {/* heading */}
+      <div className="flex items-center justify-between">
+        <h1 className={`text-md font-bold ${workSans.className}`}>
+          Stats at a glance
+        </h1>
+      </div>
+      {/* stats list */}
+      <div className="my-2 space-y-2">
+        <StatItem
+          icon={<FaChartLine className="text-green-600" />}
+          label="Monthly Average Revenue"
+          value="12,000 KES"
+        />
+        <StatItem
+          icon={<FaMoneyBillWave className="text-green-600" />}
+          label="Total Amount Received"
+          value="12,000 KES"
+        />
+        <StatItem
+          icon={<FaUsers className="text-green-600" />}
+          label="Total Clients"
+          value="12,000 KES"
+        />
+      </div>
+    </div>
+  );
 }
