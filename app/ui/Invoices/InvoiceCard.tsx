df9af1515b14f43a59a221f7512712fd71de9044
faithgaiciumia <faithgaiciumia078@gmail.com>
2025-04-26 23:07:04 +0300

    invoice card dark mode

--- app/ui/Invoices/InvoiceCard.tsx
+++ app/ui/Invoices/InvoiceCard.tsx
@@ -16,33 +16,32 @@ export default async function InvoiceCard({
   id: string;
   taskTitle: string;
 }) {
-  //function to format the currency amounts
+  // function to format the currency amounts
   const formatter = new Intl.NumberFormat("en-KE", {
     style: "currency",
     currency: "KES",
   });
-  //   creating a more readable invoice id
+
+  // creating a more readable invoice id
   const formattedID = `INV-${new Date(invoiceDate)
     .toISOString()
     .slice(0, 10)
     .replace(/-/g, "")}-${id.slice(0, 4)}`;
 
   return (
-    <div className="grid grid-cols-6 items-center justify-between gap-4 p-4 bg-white shadow rounded-lg border border-gray-100 hover:border-green-600 hover:scale-[1.02] transition-transform duration-200 ease-in-out cursor-pointer">
+    <div className="grid my-2 grid-cols-6 items-center justify-between gap-4 p-4 bg-white dark:bg-gray-900 shadow rounded-lg border border-gray-100 dark:border-gray-700 hover:border-green-600 hover:scale-[1.02] transition-transform duration-200 ease-in-out cursor-pointer">
       {/* Invoice ID */}
-      <p className={`text-sm font-semibold ${poppins.className} col-span-1`}>
-        INV-{formattedID}
+      <p className={`text-sm font-semibold text-gray-800 dark:text-white ${poppins.className} col-span-1`}>
+        {formattedID}
       </p>
 
-      {/* Client Name */}
-      <p
-        className={`text-sm font-semibold text-gray-700 ${poppins.className} col-span-1 break-words`}
-      >
+      {/* Client Email */}
+      <p className={`text-sm font-semibold text-gray-700 dark:text-gray-300 ${poppins.className} col-span-1 break-words`}>
         {clientEmail}
       </p>
 
       {/* Project Title */}
-      <p className={`text-sm text-gray-600 ${poppins.className} col-span-1`}>
+      <p className={`text-sm text-gray-600 dark:text-gray-400 ${poppins.className} col-span-1`}>
         {taskTitle}
       </p>
 
@@ -52,7 +51,7 @@ export default async function InvoiceCard({
       </div>
 
       {/* Invoice Date */}
-      <p className={`col-span-1 text-sm text-gray-600 ${poppins.className}`}>
+      <p className={`col-span-1 text-sm text-gray-600 dark:text-gray-400 ${poppins.className}`}>
         {new Date(invoiceDate).toLocaleString("en-KE", {
           year: "numeric",
           month: "short",
@@ -61,7 +60,7 @@ export default async function InvoiceCard({
       </p>
 
       {/* Amount */}
-      <p className={`col-span-1 text-sm font-semibold ${poppins.className}`}>
+      <p className={`col-span-1 text-sm font-semibold text-gray-800 dark:text-white ${poppins.className}`}>
         {formatter.format(totalAmount)}
       </p>
     </div>
