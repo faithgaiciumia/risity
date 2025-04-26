783ef50d18b3aa9cbbbbe2039c06fae6b1357823
faithgaiciumia <faithgaiciumia078@gmail.com>
2025-04-26 19:56:26 +0300

    logic to toggle light and dark mode

--- app/ui/Dashboard/LightDarkModeToggle.tsx
+++ app/ui/Dashboard/LightDarkModeToggle.tsx
@@ -1,11 +1,34 @@
+"use client";
 import { Button } from "@/components/ui/button";
-import {  Moon } from "lucide-react";
+import { Moon, Sun } from "lucide-react";
+import { useEffect, useState } from "react";
 
 export default function LightDarkModeToggle() {
+  // initialize
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  useEffect(() => {
+    //check what theme the file is in
+    if (document.documentElement.classList.contains("dark")) {
+      setIsDarkMode(true);
+    }
+  }, []);
+  const toggle = () => {
+    //get all the html elements
+    const html = document.documentElement;
+    //if in dark mode, remove dark classname
+    if (html.classList.contains("dark")) {
+      html.classList.remove("dark");
+      setIsDarkMode(false);
+    } else {
+      // if it was initially in light mode add the dark classname to the element
+      html.classList.add("dark");
+      setIsDarkMode(true);
+    }
+  };
   return (
     <div>
-      <Button variant="outline" size="icon">
-        <Moon />
+      <Button variant="outline" size="icon" onClick={toggle}>
+        {isDarkMode ? <Sun /> : <Moon />}
       </Button>
     </div>
   );
