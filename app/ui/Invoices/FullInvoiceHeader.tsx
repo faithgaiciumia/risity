5978128fd8170a66fe51e4cd7ce44c687e919851
faithgaiciumia <faithgaiciumia078@gmail.com>
2025-04-28 13:00:40 +0300

    added next link on edit button

--- app/ui/Invoices/FullInvoiceHeader.tsx
+++ app/ui/Invoices/FullInvoiceHeader.tsx
@@ -1,7 +1,9 @@
 import { FaPen, FaTrash, FaFilePdf, FaShare } from "react-icons/fa6";
 import { poppins, workSans } from "../fonts";
+import Link from "next/link";
 
 export default function FullInvoiceHeader() {
+  const invoiceID = "cc4207d8-7476-4f80-89a1-e569e4975da4";
   return (
     <div className="flex flex-col md:flex-row md:justify-between md:items-center p-4 bg-white dark:bg-gray-900 rounded-md shadow-sm">
       {/* heading */}
@@ -28,11 +30,13 @@ export default function FullInvoiceHeader() {
         </button>
 
         {/* CRUD actions */}
-        <button
-          className={`${workSans.className} flex items-center gap-1 px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white rounded-md text-sm`}
-        >
-          <FaPen /> Edit
-        </button>
+        <Link href={`/invoices/${invoiceID}/edit`}>
+          <button
+            className={`${workSans.className} flex items-center gap-1 px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white rounded-md text-sm`}
+          >
+            <FaPen /> Edit
+          </button>
+        </Link>
         <button
           className={`${workSans.className} flex items-center gap-1 px-4 py-2 bg-red-600 hover:bg-red-700 text-white rounded-md text-sm`}
         >
