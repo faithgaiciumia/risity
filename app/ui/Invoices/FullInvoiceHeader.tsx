a140cc2c80ffd3c0f04c0043f43d591c5b896020
faithgaiciumia <faithgaiciumia078@gmail.com>
2025-04-30 11:16:42 +0300

    redirect to invoices page after delete

--- app/ui/Invoices/FullInvoiceHeader.tsx
+++ app/ui/Invoices/FullInvoiceHeader.tsx
@@ -4,7 +4,8 @@ import { poppins, workSans } from "../fonts";
 import Link from "next/link";
 import { useActionState, useEffect } from "react";
 import { deleteInvoiceAction } from "@/app/lib/actions";
-import { toast } from "react-toastify";
+import { toast, ToastContainer } from "react-toastify";
+import { useRouter } from "next/navigation";
 
 export default function FullInvoiceHeader({
   invoiceID,
@@ -15,18 +16,29 @@ export default function FullInvoiceHeader({
   setIsEditing: (value: boolean) => void;
   isEditing: boolean;
 }) {
+  //router for rerouting after successful delete
+  const router = useRouter();
+
+  //handle form submission
   const [message, formAction, isPending] = useActionState(
     deleteInvoiceAction,
     undefined
   );
   // listen for a message from the action
   useEffect(() => {
-    if (message) {
-      toast(message);
+    if (message === "Invoice Deleted Successfully") {
+      toast.success(message);
+      //redirect to invoices page after 15seconds
+      setTimeout(() => {
+        router.push("/invoices");
+      }, 1500);
+    } else if (message) {
+      toast.error(message);
     }
   }, [message]);
   return (
     <div className="flex flex-col md:flex-row md:justify-between md:items-center p-4 bg-white dark:bg-gray-900 rounded-md shadow-sm">
+      <ToastContainer />
       {/* heading */}
       <div>
         <h1
