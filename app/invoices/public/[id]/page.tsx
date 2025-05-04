3bf5409767fa7fcd7b2130dd6a6b66931adb5244
faithgaiciumia <faithgaiciumia078@gmail.com>
2025-05-04 12:55:04 +0300

    correct passing of invoice saved page

--- app/invoices/public/[id]/page.tsx
+++ app/invoices/public/[id]/page.tsx
@@ -1,4 +1,6 @@
 import { getInvoiceByIdPublic } from "@/app/lib/data";
+import { formatAmount, formatDate, formatInvoiceID } from "@/app/lib/helpers";
+import FullInvoiceSaved from "@/app/ui/Invoices/FullInvoiceSaved";
 
 export default async function InvoicePublicView(props: {
     params: Promise<{ id: string }>;
@@ -9,6 +11,20 @@ export default async function InvoicePublicView(props: {
       //get the invoice
       const currentInvoice = await getInvoiceByIdPublic(id);
     return(
-        <div>heyya</div>
+        <>
+        <FullInvoiceSaved                
+                dateIssued={formatDate(currentInvoice.invoice_date)}
+                invoiceID={formatInvoiceID(
+                  currentInvoice.invoice_date,
+                  currentInvoice.id
+                )}
+                status={currentInvoice.status}
+                dueDate={formatDate(currentInvoice.due_date)}
+                taskTitle={currentInvoice.task_title}
+                userEmail={currentInvoice.user_email}
+                clientEmail={currentInvoice.client_email}
+                totalAmount={formatAmount(currentInvoice.total_amount)}                
+              />
+        </>
     )
 }
