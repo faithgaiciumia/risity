f7fdc98d3d898af224d5aed7fd265ccddc663021
faithgaiciumia <faithgaiciumia078@gmail.com>
2025-05-05 12:04:39 +0300

    added logout btn to dash nav

--- app/ui/Dashboard/DashNav.tsx
+++ app/ui/Dashboard/DashNav.tsx
@@ -2,13 +2,14 @@ import { BiCalculator } from "react-icons/bi";
 import { poppins } from "../fonts";
 import Link from "next/link";
 import LightDarkModeToggle from "./LightDarkModeToggle";
+import LogoutButton from "../Landing/LogoutButton";
 
 export default function DashNav() {
   return (
     <nav className="w-full border-b border-gray-200 bg-white dark:bg-gray-900 dark:border-gray-700 shadow">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
-          <Link href={"/"} className="hover:underline">
+          <Link href={"/dashboard"} className="hover:underline">
             <div className="flex items-center space-x-2">
               <BiCalculator className="text-green-500 text-2xl" />
               <h1
@@ -27,6 +28,7 @@ export default function DashNav() {
                 New
               </button>
             </Link>
+            <LogoutButton/>
           </div>
         </div>
       </div>
