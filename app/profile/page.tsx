78ef607f291625e2d19f39c975ad8dbb86f2a667
faithgaiciumia <faithgaiciumia078@gmail.com>
2025-05-05 15:19:15 +0300

    changed save changes btn color

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -7,7 +7,7 @@ export default function Profile() {
       <DashNav />
       <div className="flex items-center justify-between p-4">
         <h1 className={`${poppins.className} text-md font-semibold text-gray-900 dark:text-white`}>Personal Information</h1>
-        <button className="rounded-lg border border-gray-300 dark:border-gray-500 bg-green-600 text-white px-4 py-2 text-sm font-medium shadow-sm hover:bg-green-900 hover:text-white transition">
+        <button className="rounded-lg border border-gray-300 dark:border-gray-500 bg-blue-500 text-white px-4 py-2 text-sm font-medium shadow-sm hover:bg-blue-700 hover:text-white transition">
           Save Changes
         </button>
       </div>
