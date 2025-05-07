1f7cbf713937c5b50b9360bc11cc4ef2151b0212
faithgaiciumia <faithgaiciumia078@gmail.com>
2025-05-07 10:00:18 +0300

    client profile circle

--- app/ui/Clients/ClientCard.tsx
+++ app/ui/Clients/ClientCard.tsx
@@ -27,28 +27,32 @@ export default function ClientCard({
         />
       ) : (
         <div className="grid my-2 grid-cols-6 items-center justify-between gap-4 p-4 bg-white dark:bg-gray-900 shadow rounded-lg border border-gray-100 dark:border-gray-700 hover:border-green-600 hover:scale-[1.02] transition-transform duration-200 ease-in-out cursor-pointer w-full">
-          {/* Invoice ID */}
-          <p
-            className={`text-sm font-semibold text-gray-800 dark:text-white ${poppins.className} col-span-1`}
-          >
-            {companyName}
-          </p>
+          <div className="col-span-2 flex items-center gap-4">
+            {/* client profile circle */}
+            <div className="bg-blue-600 w-[30px] h-[30px] flex items-center justify-center rounded-[50%]">
+              <p className={`${poppins.className} text-sm text-white`}>F</p>
+            </div>
+            {/* client name */}
+            <p
+              className={`text-sm font-semibold text-gray-800 dark:text-white ${poppins.className}`}
+            >
+              {name}
+            </p>
+          </div>
 
           {/* Client Email */}
           <p
-            className={`text-sm font-semibold text-gray-700 dark:text-gray-300 ${poppins.className} col-span-1 break-words`}
+            className={`text-sm font-semibold text-gray-700 dark:text-gray-300 ${poppins.className} col-span-2 break-words`}
           >
             {email}
           </p>
 
-          {/* Project Title */}
+          {/* client company */}
           <p
-            className={`text-sm text-gray-600 dark:text-gray-400 ${poppins.className} col-span-1`}
+            className={`text-sm text-gray-600 dark:text-gray-400 ${poppins.className} col-span-2`}
           >
-            {name}
+            {companyName || "No company added yet."}
           </p>
-
-          
         </div>
       )}
     </>
