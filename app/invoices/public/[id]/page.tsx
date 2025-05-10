bb4758178c6707e53b601ded61fdd7f8f0ebe616
faithgaiciumia <faithgaiciumia078@gmail.com>
2025-05-10 19:49:11 +0300

    added invoice services to public invoice

--- app/invoices/public/[id]/page.tsx
+++ app/invoices/public/[id]/page.tsx
@@ -1,30 +1,33 @@
-import { getInvoiceByIdPublic } from "@/app/lib/data";
+import { getInvoiceByIdPublic, getInvoiceServices } from "@/app/lib/data";
 import { formatAmount, formatDate, formatInvoiceID } from "@/app/lib/helpers";
 import FullInvoiceSaved from "@/app/ui/Invoices/FullInvoiceSaved";
 
 export default async function InvoicePublicView(props: {
-    params: Promise<{ id: string }>;
-  }){
-    // get id
-      const params = await props.params;
-      const id = params.id;
-      //get the invoice
-      const currentInvoice = await getInvoiceByIdPublic(id);
-    return(
-        <>
-        <FullInvoiceSaved                
-                dateIssued={formatDate(currentInvoice.invoice_date)}
-                invoiceID={formatInvoiceID(
-                  currentInvoice.invoice_date,
-                  currentInvoice.id
-                )}
-                status={currentInvoice.status}
-                dueDate={formatDate(currentInvoice.due_date)}
-                taskTitle={currentInvoice.task_title}
-                userEmail={currentInvoice.user_email}
-                clientEmail={currentInvoice.client_email}
-                totalAmount={formatAmount(currentInvoice.total_amount)}                
-              />
-        </>
-    )
+  params: Promise<{ id: string }>;
+}) {
+  // get id
+  const params = await props.params;
+  const id = params.id;
+  //get the invoice
+  const currentInvoice = await getInvoiceByIdPublic(id);
+  //get invoice services
+  const invoiceServices = await getInvoiceServices(id);
+  return (
+    <>
+      <FullInvoiceSaved
+        dateIssued={formatDate(currentInvoice.invoice_date)}
+        invoiceID={formatInvoiceID(
+          currentInvoice.invoice_date,
+          currentInvoice.id
+        )}
+        status={currentInvoice.status}
+        dueDate={formatDate(currentInvoice.due_date)}
+        taskTitle={currentInvoice.task_title}
+        userEmail={currentInvoice.user_email}
+        clientEmail={currentInvoice.client_email}
+        totalAmount={formatAmount(currentInvoice.total_amount)}
+        invoiceServices={invoiceServices}
+      />
+    </>
+  );
 }
