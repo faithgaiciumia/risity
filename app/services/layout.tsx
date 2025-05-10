2108cfb6f12fbf0269c076c6abc8a91b55ae2146
faithgaiciumia <faithgaiciumia078@gmail.com>
2025-05-10 19:22:52 +0300

    hide sidebar in client new and service new

--- app/services/layout.tsx
+++ app/services/layout.tsx
@@ -1,10 +1,16 @@
+"use client";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "../ui/Dashboard/AppSidebar";
+import { usePathname } from "next/navigation";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  // logic to exclude new and public routes from sidebar
+      const pathName = usePathname();
+      const hideSidebar =
+        pathName === "/services/new";
   return (
     <SidebarProvider>
-      <AppSidebar />
+      {!hideSidebar && <AppSidebar />}
       <main className="w-full">{children}</main>
     </SidebarProvider>
   );
