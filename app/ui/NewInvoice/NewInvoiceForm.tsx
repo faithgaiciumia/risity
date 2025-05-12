8f4e75b785b9493e87376731719ee0b550478522
faithgaiciumia <faithgaiciumia078@gmail.com>
2025-05-12 21:41:59 +0300

    got rid of hook form

--- app/ui/NewInvoice/NewInvoiceForm.tsx
+++ app/ui/NewInvoice/NewInvoiceForm.tsx
@@ -1,39 +1,29 @@
 "use client";
 import { FaSpinner } from "react-icons/fa6";
 import { poppins } from "../fonts";
-import { Suspense, useActionState, useState } from "react";
+import { useActionState, useState, useEffect, Suspense } from "react";
 import { createNewInvoice } from "@/app/lib/actions";
 import { toast, ToastContainer } from "react-toastify";
-import { useEffect } from "react";
-import ClientsSelect from "./ClientsSelect";
 import { useRouter } from "next/navigation";
-import ServicesSelect from "./ServicesSelect";
-import ProjectTitleServices from "./ProjectTitleServices";
-import InvoiceClientDetails from "./InvoiceClientDetails";
-import InvoiceDueDates from "./InvoiceDueDates";
 import { useForm } from "react-hook-form";
+import ClientsSelect from "./ClientsSelect";
+import ServicesSelect from "./ServicesSelect";
 
 export default function NewInvoiceForm() {
-  //manage step by step form
-  const [step, setStep] = useState(0);
-  //router for rerouting after successful delete
+  const [amount, setAmount] = useState(0);
   const router = useRouter();
-  //handle form submission
+
+  // Capture form values
+  const { register, handleSubmit } = useForm({ shouldUnregister: false });
+
   const [message, formAction, isPending] = useActionState(
     createNewInvoice,
     undefined
   );
-  //react hook for managing form state
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (data: any) => {
-    console.log(data);
-  };
 
-  // listen for a message from the action
   useEffect(() => {
     if (message === "Invoice created successfully.") {
       toast.success(message);
-      //redirect to invoices page after 10seconds
       setTimeout(() => {
         router.push("/invoices");
       }, 1000);
@@ -43,46 +33,112 @@ export default function NewInvoiceForm() {
   }, [message]);
 
   return (
-    <form className="space-y-3" onSubmit={()=> handleSubmit(onSubmit)}>
+    <form className="space-y-6" action={formAction}>
       <ToastContainer />
-      {step === 0 && <ProjectTitleServices register={register} />}
-      {step === 1 && <InvoiceClientDetails register={register} />}
-      {step === 2 && <InvoiceDueDates register={register}/>}
 
-      {/*next, back, and save buttons */}
-      <div className="flex justify-between mt-6">
-        {step > 0 && (
-          <button
-            type="button"
-            onClick={() => setStep((prev) => prev - 1)}
-            className="px-4 py-2 bg-gray-300 dark:bg-gray-700 text-sm rounded hover:bg-gray-400"
-          >
-            Back
-          </button>
-        )}
-        {step < 2 ? (
-          <button
-            type="button"
-            onClick={() => setStep((prev) => prev + 1)}
-            className="px-4 py-2 bg-green-500 text-white text-sm rounded hover:bg-green-700"
-          >
-            Next
-          </button>
-        ) : (
-          <button
-            type="submit"
-            disabled={isPending}
-            className="w-[300px] flex items-center justify-center gap-2 rounded-lg bg-green-600 text-white px-6 py-4 text-sm font-medium shadow-sm hover:bg-green-900 transition"
-          >
-            {isPending ? (
-              <>
-                <FaSpinner className="animate-spin" /> Creating ...
-              </>
-            ) : (
-              "Create"
-            )}
-          </button>
-        )}
+      {/* === Project Title + Service Selection === */}
+      <div>
+        <label className={`mb-2 font-bold ${poppins.className}`}>
+          Task Title
+        </label>
+        <input
+          type="text"
+          {...register("title")}
+          placeholder="eg. Baraka Website"
+          className={`w-full border px-2 py-4 mt-2 border-gray-700 dark:border-gray-300 dark:bg-gray-800 dark:text-white text-sm ${poppins.className}`}
+          required
+        />
+      </div>
+
+      <div>
+        <label className={`mb-2 font-bold ${poppins.className}`}>Service</label>
+        <Suspense>
+          <ServicesSelect
+            amount={amount}
+            setAmount={setAmount}
+            register={register}
+          />
+        </Suspense>
+      </div>
+
+      <div>
+        <label className={`mb-2 font-bold ${poppins.className}`}>Amount</label>
+        <input
+          type="text"
+          defaultValue={amount}
+          {...register("amount")}
+          placeholder="2,000"
+          className={`w-full border px-2 py-4 mt-2 border-gray-700 dark:border-gray-300 dark:bg-gray-800 dark:text-white text-sm ${poppins.className}`}
+          required
+        />
+      </div>
+
+      {/* === Client Details === */}
+      <div>
+        <label className={`mb-2 font-bold ${poppins.className}`}>Client</label>
+        <Suspense>
+          <ClientsSelect register={register} />
+        </Suspense>
+      </div>
+
+      {/* === Invoice Due Dates === */}
+      <div>
+        <label className={`mb-2 font-bold ${poppins.className}`}>
+          Invoice Status
+        </label>
+        <select
+          {...register("status")}
+          className={`w-full border px-2 py-4 mt-2 border-gray-700 dark:border-gray-300 dark:bg-gray-800 dark:text-white text-sm ${poppins.className}`}
+          required
+          defaultValue=""
+        >
+          <option value="" disabled>
+            Select status
+          </option>
+          <option value="Paid">Paid</option>
+          <option value="Pending">Pending</option>
+          <option value="Overdue">Overdue</option>
+        </select>
+      </div>
+
+      <div>
+        <label className={`mb-2 font-bold ${poppins.className}`}>Date</label>
+        <input
+          type="date"
+          {...register("invoiceDate")}
+          defaultValue={new Date().toISOString().split("T")[0]}
+          className={`w-full border px-2 py-4 mt-2 border-gray-700 dark:border-gray-300 dark:bg-gray-800 dark:text-white text-sm ${poppins.className}`}
+          required
+        />
+      </div>
+
+      <div>
+        <label className={`mb-2 font-bold ${poppins.className}`}>
+          Due Date
+        </label>
+        <input
+          type="date"
+          {...register("dueDate")}
+          className={`w-full border px-2 py-4 mt-2 border-gray-700 dark:border-gray-300 dark:bg-gray-800 dark:text-white text-sm ${poppins.className}`}
+          required
+        />
+      </div>
+
+      {/* === Submit Button === */}
+      <div className="flex justify-center mt-6">
+        <button
+          type="submit"
+          disabled={isPending}
+          className="w-[300px] flex items-center justify-center gap-2 rounded-lg bg-green-600 text-white px-6 py-4 text-sm font-medium shadow-sm hover:bg-green-900 transition"
+        >
+          {isPending ? (
+            <>
+              <FaSpinner className="animate-spin" /> Creating ...
+            </>
+          ) : (
+            "Create Invoice"
+          )}
+        </button>
       </div>
     </form>
   );
