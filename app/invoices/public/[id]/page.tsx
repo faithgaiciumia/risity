b04400a4dcfd0f74be11b5530ae56212a29e9165
faithgaiciumia <faithgaiciumia078@gmail.com>
2025-05-12 22:32:16 +0300

    corrected props passing issue

--- app/invoices/public/[id]/page.tsx
+++ app/invoices/public/[id]/page.tsx
@@ -1,4 +1,9 @@
-import { getInvoiceByIdPublic, getInvoiceServices } from "@/app/lib/data";
+import {
+  getClientByEmail,
+  getInvoiceByIdPublic,
+  getInvoiceServices,
+  getUserByEmail,
+} from "@/app/lib/data";
 import { formatAmount, formatDate, formatInvoiceID } from "@/app/lib/helpers";
 import FullInvoiceSaved from "@/app/ui/Invoices/FullInvoiceSaved";
 
@@ -12,6 +17,12 @@ export default async function InvoicePublicView(props: {
   const currentInvoice = await getInvoiceByIdPublic(id);
   //get invoice services
   const invoiceServices = await getInvoiceServices(id);
+  //get client
+  const client = await getClientByEmail(currentInvoice.client_email);
+  const clientName = client.name;
+  //get user
+  const user = await getUserByEmail();
+  const userName = user.name;
   return (
     <>
       <FullInvoiceSaved
@@ -27,6 +38,9 @@ export default async function InvoicePublicView(props: {
         clientEmail={currentInvoice.client_email}
         totalAmount={formatAmount(currentInvoice.total_amount)}
         invoiceServices={invoiceServices}
+        rawTotalAmount={currentInvoice.total_amount}
+        clientName={clientName}
+        userName={userName}
       />
     </>
   );
