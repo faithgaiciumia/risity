f70edc3be56b6ff88ad50041574342584cbc44f7
faithgaiciumia <faithgaiciumia078@gmail.com>
2025-05-19 13:19:24 +0300

    resend email timer and logic set

--- app/api/auth/verify-request/page.tsx
+++ app/api/auth/verify-request/page.tsx
@@ -3,9 +3,25 @@ import { useAuthStore } from "@/app/store/useAuthStore";
 import { poppins, workSans } from "@/app/ui/fonts";
 import TopBar from "@/app/ui/Login/TopBar";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function VerifyRequest() {
   const email = useAuthStore((state) => state.email);
+  const [timer, setTimer] = useState(15);
+  const [resendEnabled, setResendEnabled] = useState(false);
+  useEffect(() => {
+    if (timer === 0) {
+      setResendEnabled(true);
+      return;
+    }
+
+    const interval = setInterval(() => {
+      setTimer((prev) => prev - 1);
+    }, 1000);
+
+    return () => clearInterval(interval); 
+  }, [timer]);
+
   return (
     <main>
       <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
@@ -24,25 +40,39 @@ export default function VerifyRequest() {
             </h1>
 
             <div className="text-center">
-              <h2 className={`${poppins.className} text-lg font-bold text-gray-800 dark:text-gray-200`}>
+              <h2
+                className={`${poppins.className} text-lg font-bold text-gray-800 dark:text-gray-200`}
+              >
                 Verification Email Sent
               </h2>
-              <p className={`${workSans.className} text-sm text-gray-600 dark:text-gray-400 my-2`}>
+              <p
+                className={`${workSans.className} text-sm text-gray-600 dark:text-gray-400 my-2`}
+              >
                 A login link has been sent to your email address. Please check
                 your inbox and click the link to proceed.
               </p>
             </div>
 
             {/* Signup Prompt */}
-            <p className={`${workSans.className} text-sm text-gray-500 dark:text-gray-300`}>
-              Didn't receive it yet?{" "}
-              <Link
-                href="/"
-                className="font-bold text-green-600 dark:text-green-400 hover:underline"
+            <form>
+              <input hidden defaultValue={email || ""} />
+              <p
+                className={`${workSans.className} text-sm text-gray-500 dark:text-gray-300`}
               >
-                Resend Email
-              </Link>
-            </p>
+                Didn't receive it yet?{" "}
+                <button
+                  type="submit"
+                  disabled={!resendEnabled}
+                  className={`font-bold ${
+                    resendEnabled
+                      ? "text-green-600 dark:text-green-400 hover:underline"
+                      : "text-gray-400 dark:text-gray-500 cursor-not-allowed"
+                  }`}
+                >
+                   Resend Email {resendEnabled ? "" : `(${timer}seconds)`}
+                </button>
+              </p>
+            </form>
           </div>
         </div>
       </div>
